0abffd3441ad7ee8e4a1fb219577374103f70b3a
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-06-12 11:17:08 +0500

    change path plasmicauthdata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,8 @@ import '@/styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import {usePlasmicAuthData} from "../utils/usePlasmicAuth";
+import {usePlasmicAuthData} from "@/utils/usePlasmicAuth";
+
 // export default function MyApp({ Component, pageProps }: AppProps) {
 //   return (
 //     <PlasmicRootProvider Head={Head}>
