63040ad92ed824ff78b9f62ef69f098d6cb26c4a
Elchinbek Jumanazarov <elchinbekjumanazarov@gmail.com>
2024-06-27 09:35:15 +0500

    create showTeam page

--- pages/team-2/[comp_id]/[id].tsx
+++ pages/team-2/[comp_id]/[id].tsx
@@ -2,25 +2,25 @@
 // This file is owned by you, feel free to edit as you see fit.
 import * as React from "react";
 import { PageParamsProvider as PageParamsProvider__ } from "@plasmicapp/react-web/lib/host";
-import GlobalContextsProvider from "../components/plasmic/proliga/PlasmicGlobalContextsProvider";
+import GlobalContextsProvider from "../../../components/plasmic/proliga/PlasmicGlobalContextsProvider";
 
-import { PlasmicShowTeams } from "../components/plasmic/proliga/PlasmicShowTeams";
+import { PlasmicShowTeam } from "../../../components/plasmic/proliga/PlasmicShowTeam";
 import { useRouter } from "next/router";
 
-function ShowTeams() {
-  // Use PlasmicShowTeams to render this component as it was
+function ShowTeam() {
+  // Use PlasmicShowTeam to render this component as it was
   // designed in Plasmic, by activating the appropriate variants,
   // attaching the appropriate event handlers, etc.  You
   // can also install whatever React hooks you need here to manage state or
   // fetch data.
   //
-  // Props you can pass into PlasmicShowTeams are:
+  // Props you can pass into PlasmicShowTeam are:
   // 1. Variants you want to activate,
   // 2. Contents for slots you want to fill,
   // 3. Overrides for any named node in the component to attach behavior and data,
   // 4. Props to set on the root node.
   //
-  // By default, PlasmicShowTeams is wrapped by your project's global
+  // By default, PlasmicShowTeam is wrapped by your project's global
   // variant context providers. These wrappers may be moved to
   // Next.js Custom App component
   // (https://nextjs.org/docs/advanced-features/custom-app).
@@ -31,10 +31,10 @@ function ShowTeams() {
         params={useRouter()?.query}
         query={useRouter()?.query}
       >
-        <PlasmicShowTeams />
+        <PlasmicShowTeam />
       </PageParamsProvider__>
     </GlobalContextsProvider>
   );
 }
 
-export default ShowTeams;
+export default ShowTeam;
